91cbfe736e66fd42089605d31db38a1955702f58
Mercy <mercycherotich757@gmail.com>
2022-03-11 17:25:51 +0300

    create blog-form,comments, form and update profile form

---
+++ app/main/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import DataRequired
+
+class CommentForm(FlaskForm):
+     comment = TextAreaField('Comment',validators = [DataRequired()])
+     submit = SubmitField('Submit')
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us more about you.',validators = [DataRequired()])
+    submit = SubmitField('Submit')
+
+class BlogForm(FlaskForm):
+     title = StringField('Title',validators = [DataRequired()])
+     pitch_text = TextAreaField('Write your blog...',validators = [DataRequired()])
+     submit = SubmitField('Submit')
