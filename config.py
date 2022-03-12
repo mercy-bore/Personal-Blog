476e1326db6fa7d004176c9a79686cf4f5cbc529
Mercy <mercycherotich757@gmail.com>
2022-03-13 00:31:39 +0300

    deploy

--- config.py
+++ config.py
@@ -31,7 +31,8 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:2015@localhost/blogs'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI  ='postgresql://txkjwgqssabmfe:d7d3418f0fdc6768abb189b94a24d7da7aac2c35d417e6832fa3a185de429206@ec2-44-195-191-252.compute-1.amazonaws.com:5432/dcfun1m7m7fbcv'
 
 
 
